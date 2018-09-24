9eaa7314f2274459969e44dfc45661543bf0998f
Hussain Ahmed <ha.alkhatib96@outlook.com>
2018-09-24 07:05:07 +0300

    title of footer been smaller

--- js/main.js
+++ js/main.js
@@ -8,4 +8,13 @@ $(function () {
         navH = $('.navbar').innerHeight();
 
     $('.slider, .carousel-item').height(winH - (navH * 3.4))
+
+ 
 });
+
+   // use fittext
+
+   $(".our-head").fitText(1.9, {
+       minFontSize: "20px",
+       maxFontSize: "22px"
+   });
